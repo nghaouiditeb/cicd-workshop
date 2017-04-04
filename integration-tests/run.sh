a35131e4a15ce702b004ad7b47d8bff8d141b6be
Ant Weiss <antweiss@users.noreply.github.com>
2017-04-04 22:43:21 +0300

    Update run.sh

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -x
+#! /bin/sh -x
 # Integration test for the product ordering application. 
 # This is not in fact an integration test. 
 # The real stuff would be adding some selenium/phantomjs tests with real UI interaction
